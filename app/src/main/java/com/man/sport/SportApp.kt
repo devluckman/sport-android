efef64f69e8178b94970706f6d9fdcf7fa6a74b0
Luckman <locktis.man@gmail.com>
2019-03-19 17:22:32 +0700

    two commit

---
+++ app/src/main/java/com/man/sport/SportApp.kt
@@ -0,0 +1,16 @@
+package com.man.sport
+
+import android.app.Application
+
+/**
+ * Create by
+ * Name    : Lukmanul Hakim
+ * on      : 19, March, 2019
+ */
+class SportApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/man/sport/model/Team.kt
@@ -0,0 +1,9 @@
+package com.man.sport.model
+
+/**
+ * Create by
+ * Name    : Lukmanul Hakim
+ * on      : 19, March, 2019
+ */
+class Team {
+}
